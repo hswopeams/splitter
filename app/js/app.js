01f4377ba669ddbf003d3f5a49b83e42e0792baa
Heather Swope <hswope@MacBook-Pro-van-Heather.local>
2019-10-07 18:23:39 +0200

    Git says I changed something. Not sure what.

--- app/js/app.js
+++ app/js/app.js
@@ -12,7 +12,7 @@ if (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined'
     window.web3 = new Web3(window.ethereum || window.web3.currentProvider);
 } else {
     // Your preferred fallback.
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); 
+   window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 }
 
 const Splitter = truffleContract(splitter);
